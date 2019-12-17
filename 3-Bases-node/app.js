243738875db4994dd6e8bc8d1f3e9547091b5224
Eduardo Caldera Coltters <e.calderac@hotmail.com>
2019-12-17 19:33:54 -0300

    Seccion Bases de Node Completada

--- 3-Bases-node/app.js
+++ 3-Bases-node/app.js
@@ -1,12 +1,37 @@
-const { crearArchivo } = require('./multiplicar/multiplicar');
+const argv = require('./config/yargs').argv;
+const colors = require('colors/safe');
+
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+
+let comando = argv._[0];
+
+
+switch (comando) {
+
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+        break;
+
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado:`, colors.green(archivo)))
+            .catch(e => console.log(e));
+        break;
+
+    default:
+        console.log('Comando no reconocido');
+
+}
 
 
 //console.log(process.argv);
-let argv = process.argv;
-let parametro = argv[2];
-let base = parametro.split('=')[1]
+// let argv2 = process.argv;
 
 
-crearArchivo(base)
-    .then(archivo => console.log(`Archivo creado: ${ archivo }`))
-    .catch(e => console.log(e));
+// console.log(argv.base);
+// console.log('Limite', argv.limite);
+
+
+//console.log(argv2);
+// let parametro = argv[2];
+// let base = parametro.split('=')[1]
--- 3-Bases-node/multiplicar/multiplicar.js
+++ 3-Bases-node/multiplicar/multiplicar.js
@@ -1,8 +1,21 @@
 //Required
 const fs = require('fs');
+const colors = require('colors');
+
+let listarTabla = (base, limite = 10) => {
+
+    console.log('================'.green);
+    console.log(`Tabla de ${ base }`.blue);
+    console.log('================'.green);
+
+    for (let i = 0; i <= limite; i++) {
+        console.log(`${ base } * ${ i } = ${ base * i }\n`);
+    }
+
+}
 
 
-crearArchivo = (base) => {
+crearArchivo = (base, limite = 10) => {
 
     return new Promise((resolve, reject) => {
 
@@ -14,17 +27,17 @@ crearArchivo = (base) => {
 
         let data = ''
 
-        for (let i = 0; i <= 10; i++) {
+        for (let i = 0; i <= limite; i++) {
             data += `${ base } * ${ i } = ${ base * i }\n`;
         }
 
         //const data = new Uint8Array(Buffer.from('Hola Mundo'));
-        fs.writeFile(`tablas/tabla-${ base }.txt`, data, (err) => {
+        fs.writeFile(`tablas/tabla-${ base }-al-${ limite }.txt`, data, (err) => {
 
             if (err)
                 reject(err)
             else
-                resolve(`tabla-${ base }.txt`)
+                resolve(`tabla-${ base }-al-${ limite }.txt`);
 
         });
 
@@ -33,5 +46,6 @@ crearArchivo = (base) => {
 }
 
 module.exports = {
-    crearArchivo
+    crearArchivo,
+    listarTabla
 }
